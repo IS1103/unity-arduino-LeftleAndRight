5f31906e4101ee978a17639a1e6125dd56329fa5
is <is100602@gmail.com>
2021-09-06 08:16:33 +0800

    init

---
+++ unity-arduino-LeftleAndRight/Assets/arduino/divice/divice.ino
@@ -0,0 +1,50 @@
+#define TRIGPIN_1 12
+#define ECHOPIN_1 11
+#define TRIGPIN_2 5
+#define ECHOPIN_2 4
+
+//取得超音波的距離
+long getDistance(int trigpin, int echopin)
+{
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(2); //    超音波 Trig  low 2us
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10); //    超音波 Trig Hi  發射信號 10us
+  digitalWrite(trigpin, LOW);
+  return pulseIn(echopin, HIGH) / 58; //  超音波 Echo 接收回傳信號計算距離
+}
+void setup()
+{
+  pinMode(TRIGPIN_1, OUTPUT);   //   Trig  為 輸出腳
+  pinMode(ECHOPIN_1, INPUT);    //   Echo  為 輸入腳
+  pinMode(TRIGPIN_2, OUTPUT);   //   Trig  為 輸出腳
+  pinMode(ECHOPIN_2, INPUT);    //   Echo  為 輸入腳
+
+  Serial.begin(9600); //開啟監控視窗
+}
+String out;
+long leftNum, rightNum;
+void loop()
+{
+  while (true) {
+    out = "";
+    delay(1000);
+
+    leftNum = getDistance(TRIGPIN_1, ECHOPIN_1);
+    if (leftNum > 0 && leftNum < 100) {
+      out = "L/" + String(leftNum);
+      Serial.println(out);
+      leftNum = 0;
+      
+    }
+
+    rightNum = getDistance(TRIGPIN_2, ECHOPIN_2);
+    
+    if (rightNum > 0 && rightNum < 100) {
+      out = "R/" + String(rightNum);
+      Serial.println(out);
+      rightNum = 0;
+    }
+
+  }
+}
---
+++ unity-arduino-LeftleAndRight/Assets/sc/Actor.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Threading;
+
+public class Actor : MonoBehaviour
+{
+    public float speed;
+
+    private SerialPort arduinoStream;
+    public string port;
+    private Thread readThread; // 宣告執行緒
+    static public string readMessage; //L、R
+
+    void Start()
+    {
+        if (!string.IsNullOrEmpty(port))
+        {
+            arduinoStream = new SerialPort(port, 9600);
+            try
+            {
+                arduinoStream.Open(); //開啟SerialPort連線
+                readThread = new Thread(new ThreadStart(ArduinoRead)); //實例化執行緒與指派呼叫函式
+                readThread.Start(); //開啟執行緒
+                Debug.Log("SerialPort開啟連接");
+            }
+            catch (Exception ex)
+            {
+                Debug.Log("SerialPort連接失敗");
+            }
+        }
+    }
+
+    private void ArduinoRead()
+    {
+        while (arduinoStream.IsOpen)
+        {
+            try
+            {
+                readMessage = arduinoStream.ReadLine(); // 讀取SerialPort資料並裝入readMessage
+
+                string[] sArray = readMessage.Split('/');
+
+                if (sArray.Length==2)
+                {
+                    Debug.Log("----->"+sArray[0]);
+                    readMessage = sArray[0];
+                }
+
+                //Debug.Log(readMessage);
+            }
+            catch (Exception e)
+            {
+                Debug.LogWarning(e.Message);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (GameFrame.playing)
+        {
+            if (Input.GetKey("left") || readMessage == "L") {
+                if (transform.localPosition.x >-10)
+                {
+                    transform.Translate(-1 * speed, 0, 0);
+                }
+            }
+            if (Input.GetKey("right") || readMessage == "R") {
+                if (transform.localPosition.x < 10)
+                {
+                    transform.Translate(1 * speed, 0, 0);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "obstacle")
+        {
+            GameFrame.onTriggerObstacle?.Invoke();
+        }
+        else if (other.tag == "end")
+        {
+            GameFrame.onTriggerFinish?.Invoke();
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        if (arduinoStream != null)
+        {
+            if (arduinoStream.IsOpen)
+            {
+                arduinoStream.Close();
+            }
+        }
+    }
+}
